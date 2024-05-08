8d94fac79b390bce6496d6faa37327d5f3786162
NathanTzChung <ntchung@ucsd.edu>
2024-05-07 22:56:20 -0700

    Fixing speech synthesis
    - was making repeat sounds

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,28 +4,30 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+
+  const synth = window.speechSynthesis;
+  const btnPress = document.querySelector("button");
+  const txtSpace = document.getElementById("text-to-speak");
+  const voiceOpts = document.getElementById("voice-select");
+  const smileImage = document.querySelector('img[alt="Smiling face"]');
+  let voices;
+  
   populateVoiceList();
   if (typeof speechSynthesis !== "undefined" &&
     speechSynthesis.onvoiceschanged !== undefined){
     speechSynthesis.onvoiceschanged = populateVoiceList;
   }
-  
+
   // Code below referenced from https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesis/getVoices
   function populateVoiceList(){
     if (typeof speechSynthesis === "undefined") {
       return;
     }
-
-    const synth = window.speechSynthesis;
-    const btnPress = document.querySelector("button");
-    const txtSpace = document.getElementById("text-to-speak");
-    const voiceOpts = document.getElementById("voice-select");
-    const smileImage = document.querySelector('img[alt="Smiling face"]');
-
+    
     let availVoices = [];
     availVoices = synth.getVoices();
 
-    // console.log("These are the avail voices: ", availVoices);
+    console.log("These are the avail voices at start: ", availVoices);
 
     // This loop is used to get list of available voices
     for (let i = 0; i < availVoices.length; i++) {
@@ -41,39 +43,41 @@ function init() {
       voiceOpts.appendChild(option);
     };
 
-    // Uses speech synthesis and speak to play sound using the selected voice
-    btnPress.addEventListener("click", function(e){
-      e.preventDefault();
+    voices = availVoices;
 
-      const speakThis = new SpeechSynthesisUtterance(txtSpace.value); // Gets the text to speak aloud
-      const selectedVoice = voiceOpts.selectedOptions[0].getAttribute("data-name");
-
-      for (let i = 0; i < availVoices.length; i++){
-        if (availVoices[i].name == selectedVoice){
-          console.log("This is the avail voice: ", availVoices[i]);
-          console.log("This is the avail voice name: ", availVoices[i].name);
-          console.log("This is the speaking voice voice: ", selectedVoice);
-          speakThis.voice = availVoices[i];
-        };
-      }
-
-      synth.cancel();
-      synth.speak(speakThis);
-
-      // Keeps track of when to change smiley face
-      speakThis.addEventListener("start", function(){
-        // console.log("speaking in progress: ", synth.speaking);
-        smileImage.src = "./assets/images/smiling-open.png"
-      })
-
-      speakThis.addEventListener("end", function(){
-        // console.log("speaking in progress: ", synth.speaking);
-        smileImage.src = "./assets/images/smiling.png"
-      })
-      
-      
-    });
   }
+
+  // Uses speech synthesis and speak to play sound using the selected voice
+  btnPress.addEventListener("click", function(e){
+    e.preventDefault();
+
+    const speakThis = new SpeechSynthesisUtterance(txtSpace.value); // Gets the text to speak aloud
+    const selectedVoice = voiceOpts.selectedOptions[0].getAttribute("data-name");
+
+    for (let i = 0; i < voices.length; i++){
+      if (voices[i].name == selectedVoice){
+        console.log("This is the avail voice: ", voices[i]);
+        console.log("This is the avail voice name: ", voices[i].name);
+        console.log("This is the speaking voice voice: ", selectedVoice);
+        speakThis.voice = voices[i];
+      };
+    }
+
+    // synth.cancel();
+    synth.speak(speakThis);
+
+    // Keeps track of when to change smiley face
+    speakThis.addEventListener("start", function(){
+      // console.log("speaking in progress: ", synth.speaking);
+      smileImage.src = "./assets/images/smiling-open.png"
+    })
+
+    speakThis.addEventListener("end", function(){
+      // console.log("speaking in progress: ", synth.speaking);
+      smileImage.src = "./assets/images/smiling.png"
+    })
+    
+  });
 
 };
 
